672ec6c314f69f7b844b102d3b5e8d90e12b817e
Prasad Mallampati <prasadmallampati16@gmail.com>
2026-05-19 19:20:00 +0530

    DDL - CREATE (#10)

--- DDL/create_exampple.sql
+++ DDL/create_exampple.sql
@@ -3,10 +3,33 @@
 --  WHY CREATE INTRODUCED BEFORE DOING ANY OPERATION LIKE INSERT OR ANY WE NEED DATABASE OR TABLE STRUCTURE  or 
 -- Before inserting any data, the table structure must exist.
 
--- WHERE TO USE : we can use while creation of database or table 
+-- Why Introduced this 
+
+-- To build new tables, databases, views, etc.
+
+-- Where to Use
+-- Creating tables
+-- Creating databases
+-- Starting a new project
 
 -- WHEN TO USE : When setting up a new database or adding a new entity or table 
 
 -- syntax
 
 -- create table table_name(column1 datatype constraint, column2 datatype)
+
+-- create database  database_name
+--  create table table_name(column1 datatype constraints,column2 datatype constraint ...)
+
+
+
+--  example for creating database
+
+
+-- example for creating table 
+
+create table empdummy2(emp_name VARCHAR(10),emp_id VARCHAR(10),emp_age int,emp_salary decimal(12,2));
+
+
+
+select * from empdummy2;
