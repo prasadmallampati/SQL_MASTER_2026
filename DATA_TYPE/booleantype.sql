677050cfebda9185f82660d74e6ff54a31d4f9fa
Prasad Mallampati <prasadmallampati16@gmail.com>
2026-05-19 16:59:35 +0530

    DATA TYPE - BOOLEAN (#7)

---
+++ DATA_TYPE/booleantype.sql
@@ -0,0 +1,16 @@
+-- Boolean type : True False
+-- Used to store true/false values.
+
+
+-- | Data Type                | Description          |
+-- | ------------------------ | -------------------- |
+-- | `BOOLEAN` / `TINYINT(1)` | Stores TRUE or FALSE |
+
+
+
+create table booleandemo(
+
+    student_id int,
+    isalive BIT,
+    name VARCHAR(20)
+);
