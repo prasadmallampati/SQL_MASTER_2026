fc9559db9471f44b498ee8c17188bdeb306bdd62
Prasad Mallampati <prasadmallampati16@gmail.com>
2026-05-19 14:28:16 +0530

    SQL - WHAT IS DB AND TABLE (#2)

---
+++ 011_database_table.sql
@@ -0,0 +1,35 @@
+--  TOPIC : DATABASE AND TABLES
+
+-- WHAT IS DATABASE :: DATABASE IS LIKE FOLDER THAT HOLDS MANY TABLES
+-- A TABLES IS LIKE EXCEL SHEET RECORDS (ROWS) AND COLUMNS(FIELDS) 
+-- EXAMPLE :: A school database contains students,teachers,subjects 
+
+--  why it was introduced :: 
+--  we need structured way to stor, easily search and manage COMMENT
+
+
+
+-- where to use use 
+--  every project need start with database and tables 
+
+--  how to use 
+
+-- Create a database
+CREATE DATABASE school;
+go
+-- Use that database
+USE school;
+go
+
+-- Create a table
+CREATE TABLE students (
+  id INT,
+  name VARCHAR(50),
+  age INT
+);
+
+
+
+--  When to use?
+-- At the very beginning of any project — before inserting or querying data.
+
