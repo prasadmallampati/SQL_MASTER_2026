264c6d5219aefdc42a67f4ba5131d4c5f35e927c
Prasad Mallampati <prasadmallampati16@gmail.com>
2026-05-19 15:37:25 +0530

    DDL - INTRO (#3)

---
+++ DDL/ddl_intro.sql
@@ -0,0 +1,8 @@
+--  TOPIC : DDL INTRO : DATA DEFINITION LANGUAGE 
+
+-- suppose we want create database,table ,or we need alter existing table , we want drop or truncate 
+
+-- 1 create table or database 
+-- 2 alter existing table
+-- 3 drop tables
+-- 4 truncate tables 
