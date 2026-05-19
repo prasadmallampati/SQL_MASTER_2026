3b655ea9f17f537aeab11d449563349519e0764d
Prasad Mallampati <prasadmallampati16@gmail.com>
2026-05-19 13:43:30 +0530

    SQL - INTRO (#1)

---
+++ 01_intro_sql.sql
@@ -0,0 +1,39 @@
+-- TOPIC SQL :: STRUCTURED QUERY LANGUAGE WHICH IS USED TO COMMUNICATE WITH DATABASE
+
+-- WHY IT WAS INTRODUCED  :: 
+-- 1 Before SQL, databases were hard to use and required complex programming.
+-- 2 SQL made it simple — even non-programmers can read and write SQL queries.
+
+
+--  Where to use?
+-- Used everywhere: websites, apps, banks, hospitals, schools — anywhere data is stored in a database 
+-- (MySQL, PostgreSQL, Oracle, SQL Server, SQLite).
+
+
+
+-- How to  Use?
+-- using select * from table_name or select column1,column2 from table_name;
+
+
+-- where to use
+-- we can use wherever we can create,update,delete,update data from database
+
+-- what is difference between sql and no sql
+
+-- sql structured query language also (Relational  Database) under tool mysql,postgres,mssql server
+
+-- nosql :: non relation databases can store flexible format of data
+-- like : document ,key:value pairs
+-- databases are mango db ,radis ,apache casandra 
+
+-- | Feature        | SQL                     | NoSQL                                 |
+-- | -------------- | ----------------------- | ------------------------------------- |
+-- | Data Structure | Tables                  | Documents / Key-Value / Graph         |
+-- | Schema         | Fixed                   | Flexible                              |
+-- | Scalability    | Vertical scaling        | Horizontal scaling                    |
+-- | Query Language | SQL                     | Varies                                |
+-- | Transactions   | Strong ACID support     | Usually flexible                      |
+-- | Best For       | Banking, ERP, analytics | Real-time apps, big data, social apps |
+
+
+
