5a65d9815d03db4fbd776abef81293aa9a06ddda
Prasad Mallampati <prasadmallampati16@gmail.com>
2026-05-19 16:54:59 +0530

    database and table (#2)

--- 011_database_table.sql
+++ 011_database_table.sql
@@ -1,38 +1,38 @@
---  TOPIC : DATABASE AND TABLES
+-- --  TOPIC : DATABASE AND TABLES
 
--- WHAT IS DATABASE :: DATABASE IS LIKE FOLDER THAT HOLDS MANY TABLES
--- A TABLES IS LIKE EXCEL SHEET RECORDS (ROWS) AND COLUMNS(FIELDS) 
--- EXAMPLE :: A school database contains students,teachers,subjects 
+-- -- WHAT IS DATABASE :: DATABASE IS LIKE FOLDER THAT HOLDS MANY TABLES
+-- -- A TABLES IS LIKE EXCEL SHEET RECORDS (ROWS) AND COLUMNS(FIELDS) 
+-- -- EXAMPLE :: A school database contains students,teachers,subjects 
 
---  why it was introduced :: 
---  we need structured way to stor, easily search and manage COMMENT
-
-
-
--- where to use use 
---  every project need start with database and tables 
-
---  how to use 
-
--- Create a database
-CREATE DATABASE school;
--- Use that database
-USE school;
-
--- Create a table
-CREATE TABLE students (
-  id INT,
-  name VARCHAR(50),
-  age INT
-);
+-- --  why it was introduced :: 
+-- --  we need structured way to stor, easily search and manage COMMENT
 
 
 
---  When to use?
--- At the very beginning of any project — before inserting or querying data.
+-- -- where to use use 
+-- --  every project need start with database and tables 
 
-select * from students;
+-- --  how to use 
+
+-- -- Create a database
+-- CREATE DATABASE school;
+-- -- Use that database
+-- USE school;
+
+-- -- Create a table
+-- CREATE TABLE students (
+--   id INT,
+--   name VARCHAR(50),
+--   age INT
+-- );
 
 
 
-select * from students;
+-- --  When to use?
+-- -- At the very beginning of any project — before inserting or querying data.
+
+-- select * from students;
+
+
+
+-- select * from students;
