9eb32a4db948815969567b273ee01701bbbafe5f
Prasad Mallampati <prasadmallampati16@gmail.com>
2026-05-19 16:11:16 +0530

    SQL - DATA TYPES (number,str,boolean,datetime and other type(#4)

---
+++ DATA_TYPE/datatypes.sql
@@ -0,0 +1,66 @@
+--  TOPIC : DATA TYPES IN SQL
+--  WHY : WITHOUT DATA TYPE THE DATA SHOULD BE USE MEANING FULL 
+
+
+-- we have several data types those  are given below 
+-- Numeric (5)
+-- INT, BIGINT, DECIMAL, FLOAT/DOUBLE, TINYINT/SMALLINT
+--  String / Text (3)
+-- VARCHAR, CHAR, TEXT
+--  Date & Time (4)
+-- DATE, DATETIME, TIMESTAMP, TIME
+--  Boolean (1)
+-- BOOLEAN / TINYINT(1)
+-- Others (2)
+-- ENUM, BLOB
+
+
+
+
+
+-- under number type we have 
+
+-- | Data Type              | Description                       |
+-- | ---------------------- | --------------------------------- |
+-- | `INT`                  | Stores whole numbers              |
+-- | `BIGINT`               | Stores very large whole numbers   |
+-- | `DECIMAL(p,s)`         | Stores exact decimal values       |
+-- | `FLOAT` / `DOUBLE`     | Stores approximate decimal values |
+-- | `TINYINT` / `SMALLINT` | Stores smaller integer values     |
+
+
+
+--  under string 
+-- | Data Type    | Description            |
+-- | ------------ | ---------------------- |
+-- | `VARCHAR(n)` | Variable-length string |
+-- | `CHAR(n)`    | Fixed-length string    |
+-- | `TEXT`       | Large text data        |
+
+
+
+--  under datetime 
+
+-- | Data Type   | Description             |
+-- | ----------- | ----------------------- |
+-- | `DATE`      | Stores only date        |
+-- | `DATETIME`  | Stores date and time    |
+-- | `TIMESTAMP` | Stores timestamp values |
+-- | `TIME`      | Stores only time        |
+
+
+
+--  under boolean 
+
+
+-- | Data Type                | Description          |
+-- | ------------------------ | -------------------- |
+-- | `BOOLEAN` / `TINYINT(1)` | Stores TRUE or FALSE |
+
+
+
+--  under other types 
+-- | Data Type | Description                                |
+-- | --------- | ------------------------------------------ |
+-- | `ENUM`    | Stores one value from a predefined list    |
+-- | `BLOB`    | Stores binary large objects (images/files) |
