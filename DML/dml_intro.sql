fc8dcbed39b8c38c30b8156e0e5a0818c3e702d8
Prasad Mallampati <prasadmallampati16@gmail.com>
2026-05-20 11:10:34 +0530

    DML - INTRO (#14)

---
+++ DML/dml_intro.sql
@@ -0,0 +1,6 @@
+-- dml :: data manipulation language (which is used insert,update,delete)
+
+
+-- insert :: user data into table
+-- update :: update data into existing table
+-- delete :: delete data from existing table 
