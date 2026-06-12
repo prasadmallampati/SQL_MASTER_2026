281ad0fed4844cb64288f8715542a8121f2e8039
PRASAD\prasa <prasadmallampati16@gmail.com>
2026-06-12 15:44:02 +0530

    DDL - INTRO (#3)

--- DML/dml_intro.sql
+++ DML/dml_intro.sql
@@ -1,6 +1,35 @@
--- dml :: data manipulation language (which is used insert,update,delete)
+-- ==========================================================
+-- DML (Data Manipulation Language)
+-- ==========================================================
+
+-- Purpose:
+-- Used to insert, update and delete data.
+
+-- INSERT -> Adds new employee.
+
+INSERT INTO Employee
+VALUES(14,'Ramesh',70000,101,1,'2025-01-10');
+
+-- UPDATE -> Modifies employee details.
+
+UPDATE Employee
+SET Salary=65000
+WHERE Emp_ID=2;
+
+-- DELETE -> Removes employee record.
+
+DELETE FROM Employee
+WHERE Emp_ID=14;
+
+-- MERGE -> Insert or Update data.
+
+-- Interview:
+-- DML works on TABLE DATA.
 
 
--- insert :: user data into table
--- update :: update data into existing table
--- delete :: delete data from existing table 
+
+-- DDL -> Design the Table
+-- DML -> Modify the Data
+-- DQL -> Query the Data
+-- DCL -> Control User Access
+-- TCL -> Transaction Control
